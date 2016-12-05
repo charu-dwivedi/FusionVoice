753d7d13fd0960453ee47c6e59f862e591e10ef8
Charu Dwivedi <charudwivedi455@gmail.com>
2016-12-05 12:31:11 -0500

    Added UI button

---
+++ FusionVoice.py
@@ -0,0 +1,119 @@
+import sys
+sys.path.append("/usr/local/lib/python3.5/site-packages/")
+from .LangProcess import nlpparse
+from .LangProcess import speech
+from .FusionVC import CommandSelect as cs
+import adsk.core, adsk.fusion, adsk.cam, traceback
+
+handlers = []
+
+def run(context):
+
+    '''
+    app = adsk.core.Application.get()
+    ui  = app.userInterface  
+    cmdDefs = ui.commandDefinitions
+    nav = ui.toolbars.itemById('NavToolbar')
+
+    buttonExample = cmdDefs.addButtonDefinition('VoiceAssistant', 'VC Button', 'VC button tooltip', \
+                                                 './/Resources//FusionVC')
+
+    buttonExampleCreated = prompt_command()
+    buttonExample.commandCreated.add(buttonExampleCreated)
+    handlers.append(buttonExampleCreated)
+
+
+    buttonControl = addInsPanel.controls.addCommand(buttonExample)
+
+    # Make the button available in the panel.
+    buttonControl.isPromotedByDefault = True
+    buttonControl.isPromoted = True
+
+    '''
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+
+        # Get the CommandDefinitions collection.
+        cmdDefs = ui.commandDefinitions
+        
+        # Create a button command definition.
+        buttonSample = cmdDefs.addButtonDefinition('FusionVC2', 
+                                                   'FusionVC', 
+                                                   'Fusion Voice Control')
+                                                   #'./Resources/Sample')
+        
+        # Connect to the command created event.
+        sampleCommandCreated = SampleCommandCreatedEventHandler()
+        buttonSample.commandCreated.add(sampleCommandCreated)
+        handlers.append(sampleCommandCreated)
+        
+        # Get the ADD-INS panel in the model workspace. 
+        addInsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        
+        # Add the button to the bottom of the panel.
+        buttonControl = addInsPanel.controls.addCommand(buttonSample)
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
+
+# Event handler for the commandCreated event.
+class SampleCommandCreatedEventHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandCreatedEventArgs.cast(args)
+        cmd = eventArgs.command
+
+        # Connect to the execute event.
+        onExecute = SampleCommandExecuteHandler()
+        cmd.execute.add(onExecute)
+        handlers.append(onExecute)
+
+
+# Event handler for the execute event.
+class SampleCommandExecuteHandler(adsk.core.CommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        eventArgs = adsk.core.CommandEventArgs.cast(args)
+
+        # Code to react to the event.
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        prompt_command()
+        ui.messageBox('In command execute event handler.')
+
+
+
+def stop(context):
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        
+        # Clean up the UI.
+        cmdDef = ui.commandDefinitions.itemById('MyButtonDefIdPython')
+        if cmdDef:
+            cmdDef.deleteMe()
+            
+        addinsPanel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        cntrl = addinsPanel.controls.itemById('MyButtonDefIdPython')
+        if cntrl:
+            cntrl.deleteMe()
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc())) 
+  
+
+def prompt_command():
+    try:
+        command = speech.speechrec() #'draw a circle with a radius of 16 inches'
+        print('draw a circle with a radius of 16 inches')
+        command_list = nlpparse.parse_sentence(command)
+        cs.run_command(command_list)
+    except ValueError as e:
+        print('The machine does not seem to recognize some of the words in the command, try again.')
+        print(e)
+        
